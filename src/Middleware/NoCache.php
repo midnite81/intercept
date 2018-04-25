4661af90d9d66ed6505cfccaa2ee1935293f0257
Simon Rogers <midnite81@users.noreply.github.com>
2018-04-25 17:01:28 +0100

    Update to method

--- src/Middleware/NoCache.php
+++ src/Middleware/NoCache.php
@@ -16,11 +16,10 @@ class NoCache
      */
     public function handle($request, Closure $next)
     {
-
-        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-        header("Cache-Control: post-check=0, pre-check=0", false);
-        header("Pragma: no-cache");
-
-        return $next($request);
+         return $next($request)->header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+                               ->header('Cache-Control', 'post-check=0, pre-check=0', false)
+                               ->header('Pragma', 'no-cache');
     }
+    
+    
 }
